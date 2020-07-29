0958decbdb8e5165aa245ac3e4b108971c1dcc5e
DanteGivens <67976030+DanteGivens@users.noreply.github.com>
2020-07-29 15:32:46 -0400

    Add files via upload

---
+++ Dante_G_Comicbook_Excercise/Superhero comic book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Superhero comic book
+//
+//  Created by Dante Givens on 7/29/20.
+//  Copyright © 2020 Dante Givens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
